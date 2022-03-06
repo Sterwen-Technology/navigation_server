67318a940e08bf1e30190bcf6cd639fa6f2fa179
laurent <laurent.carre@sterwen-technology.eu>
2022-03-06 22:24:45 +0100

    improvement on SimulatorInput

--- simulator_input.py
+++ simulator_input.py
@@ -20,3 +20,20 @@ class SimulatorInput(IPInstrument):
 
     def __init__(self, opts):
         super().__init__(opts)
+        self._max_timeouts = opts.get('max_timeouts', 3)
+        self._reopen_on_timeout = opts.get('reopen_on_timeout', True)
+        self._nb_timeout = 0
+
+    def read(self):
+        try:
+            return super().read()
+        except InstrumentTimeOut:
+            self._nb_timeout += 1
+            if self._nb_timeout > self._max_timeouts:
+                if self._reopen_on_timeout:
+                    self.close()
+            raise
+
+    def open(self):
+        self._nb_timeout = 0
+        return super().open()
