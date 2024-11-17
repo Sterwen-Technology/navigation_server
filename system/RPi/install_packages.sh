d135f3af592210e34a0c8d50be4bf418b399c43f
laurent <laurent.carre@sterwen-technology.eu>
2024-11-17 15:18:37 +0100

    Version 2.1 - with installation tested

---
+++ system/RPi/install_packages.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+#  That file install all packages on Rpi via apt
+#  shall be run as sudo
+
+# apt install python3-pip
+apt install python3-can
+apt install python3-yaml
+apt install python3-grpcio
